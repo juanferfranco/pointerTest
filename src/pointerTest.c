df016cd6ea09a4a4018315fe9f3d113e0c01c561
Juan Franco <juanferfranco@gmail.com>
2021-01-21 16:35:06 -0500

    blank project: setup

---
+++ src/pointerTest.c
@@ -0,0 +1,22 @@
+/*
+ ============================================================================
+ Name        : pointerTest.c
+ Author      : JuanFranco
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+
+
+
+
+
+	return EXIT_SUCCESS;
+}
